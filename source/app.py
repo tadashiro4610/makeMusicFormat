c200a5cc08db64a9258c113715928e48d8a9d978
tadashiro4610 <tadashiro4610@gmail.com>
2023-05-21 01:48:38 +0900

    add comments
    comments
    only exe
    name changed (Song->Music)

--- source/app.py
+++ source/app.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import colorchooser,filedialog,font
-import makeSongFormat_app
+import makeMusicFormat_app
 
 class songFormat:
     def __init__(self,master):
@@ -8,21 +8,25 @@ class songFormat:
         self.master.title("Make Music Format")
         self.master.minsize(400,300)
 
+        #excel生成用に渡す引数
         self.musicName=None
         self.barLow=None
+        self.dir=None
         self.section_entry_list=[]
         self.bar_entry_list=[]
         self.col16_list=[]
+        #表示する色の初期化
         self.col16=["tmp","#80ff80"]
-        self.dir=None
 
-        
+        #リストの初期化
         def clear_list():
             self.section_entry_list=[]
             self.bar_entry_list=[]
             self.col16_list=[]
 
+        ###event処理達
         def musicName_event():
+            #題名と一行の小節数を取得
             self.musicName=self.musicName_entry.get()
             self.barLow=self.barLow_entry.get()
 
@@ -34,9 +38,12 @@ class songFormat:
             self.color_button.config(text=self.col16[1])
         
         def plus_event():
+            #セクション名、小節数、色の取得
             a=self.section_entry.get()
             b=self.bar_entry.get()
             c=self.col16[1]
+            #openpyxlだと#がいらない
+            #"#000000"だと"0"になる
             if(c!="0"):
                 c=c.split("#")
                 c=c[1]
@@ -45,9 +52,11 @@ class songFormat:
             self.section_entry.delete(0,tk.END)
             self.bar_entry.delete(0,tk.END)
             txt=a+","+b+","+c
+            #リストボックスに追加
             self.listbox.insert(tk.END,txt)
 
         def minus_event():
+            #選択物を取得して削除
             selectedIndex = tk.ACTIVE
             self.listbox.delete(selectedIndex)
 
@@ -63,7 +72,8 @@ class songFormat:
             
             musicName_event()
             
-            m=makeSongFormat_app.msf(self.musicName,self.barLow,self.section_entry_list,self.bar_entry_list,self.col16_list,self.dir)
+            #excelシートの生成
+            m=makeMusicFormat_app.msf(self.musicName,self.barLow,self.section_entry_list,self.bar_entry_list,self.col16_list,self.dir)
             m.wright()
 
         def select_file():
@@ -75,9 +85,9 @@ class songFormat:
                 self.dir=selected_file
                 self.dir_label.config(text=selected_file)
 
-        
-        def callback(event):
-            info = event.widget.grid_info()
+        #gridの行列はこれで取得できる(デバッグ用)
+        # def callback(event):
+        #     info = event.widget.grid_info()
 
 
         #widget
@@ -101,13 +111,13 @@ class songFormat:
         self.minas_button=tk.Button(text="-",width=2,height=1,command=minus_event)
         self.generate_button=tk.Button(text="generate",command=generate_event)
         self.selectFile_button=tk.Button(text="参照",command=select_file)
-        #scroll
+        #scroll,listbox
         self.scr=tk.Scrollbar(orient="vertical")
         self.list_font=font.Font(size=20)
         self.listbox=tk.Listbox(font=self.list_font)
         self.listbox["yscrollcommand"] = self.scr.set
         self.scr["command"]=self.listbox.yview
-
+        #gridで配置
         self.name_label.grid(row=0,column=0)
         self.musicName_entry.grid(row=1,column=0,sticky="nsew",padx=2)
         self.barLow_label.grid(row=0,column=1)
@@ -132,7 +142,7 @@ class songFormat:
         self.master.grid_columnconfigure(3,weight=1,minsize=100)    
         self.master.grid_rowconfigure(5,weight=1,minsize=100)
 
-        self.master.bind_all("<1>",callback)#複数のlabelを使用するため
+        # self.master.bind_all("<1>",callback) #gridの行列取得
 
 
 if __name__ == '__main__':
